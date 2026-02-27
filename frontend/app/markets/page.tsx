a5ff3cdfd29d51352838bf4f50baa1d54a4e4adc
Olowodarey <olowodarey8@gmail.com>
2026-02-27 22:53:17 +0100

    feat(markets): build page layout UI segment (part 1)

---
+++ frontend/app/markets/page.tsx
@@ -0,0 +1,5 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import MarketsSection from "../../components/MarketsSection";
+import UnifiedBackground from "../../components/UnifiedBackground";
+
