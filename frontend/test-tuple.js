308dca18939bda05e914c61ea37af5c8dc67334d
Olowodarey <olowodarey8@gmail.com>
2026-03-11 12:41:12 +0100

    remove un used file

--- frontend/test-tuple.js
+++
@@ -1,4 +0,0 @@
-const { tupleCV, uintCV } = require('@stacks/transactions');
-const t = tupleCV({ "stx-balance": uintCV(100) });
-console.log("tuple.data:", !!t.data);
-console.log("tuple.value:", !!t.value);
